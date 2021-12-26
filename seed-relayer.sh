0e6ad19e308d878fbd9a84e10b5582037a811c14
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-26 18:19:24 +0100

    Add seed lum

--- seed-relayer.sh
+++ seed-relayer.sh
@@ -6,4 +6,4 @@ echo 'Initializing environment...'
 . ibc-testbed/.env
 
 echo 'Sending 1000 LUM to relayer...'
-lumd tx bank send $(lumd keys show $IBC_KEY -a --home $LUMD_HOME --keyring-backend test) $(rly keys show $LUM_CHAIN_ID $RLY_KEY --home $RELAYER_HOME) 1000000000ulum --chain-id $LUM_CHAIN_ID --node $LUM_RPC
+lumd tx bank send $(lumd keys show $IBC_KEY -a --home $LUMD_HOME --keyring-backend test) $(rly keys show $LUM_CHAIN_ID $RLY_KEY --home $RELAYER_HOME) 1000000000ulum --chain-id $LUM_CHAIN_ID --home $LUMD_HOME --keyring-backend test --node $LUM_RPC --yes
