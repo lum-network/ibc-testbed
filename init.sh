76442e1baf2ffe915f61afb173dc57f974a12673
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-26 14:54:20 +0100

    Add genesis generation

--- init.sh
+++ init.sh
@@ -2,8 +2,14 @@
 ## Initialize all networks
 ##
 
-IBC_TESDBED_HOME=$HOME/.ibc-testbed
+export IBC_TESDBED_HOME=$HOME/.ibc-testbed
 echo 'Testbed directory is '$IBC_TESDBED_HOME
+
+echo 'Stopping all networks binaries...'
+sudo systemctl stop lumd
+sudo systemctl stop osmosisd
+sudo systemctl stop kid
+sudo systemctl stop gaiad
 
 echo 'Cleaning up testbed directory...'
 rm -rf $HOME/.ibc-testbed
@@ -13,3 +19,9 @@ lumd keys add ibc-testbed-key --home $IBC_TESDBED_HOME/lumd --keyring-backend te
 osmosisd keys add ibc-testbed-key --home $IBC_TESDBED_HOME/osmosisd --keyring-backend test
 kid keys add ibc-testbed-key --home $IBC_TESDBED_HOME/kid --keyring-backend test
 gaiad keys add ibc-testbed-key --home $IBC_TESDBED_HOME/gaiad --keyring-backend test
+
+echo 'Initializing networks genesis files...'
+cp ibc-testbed/lumd.json $IBC_TESDBED_HOME/lumd/config/genesis.json
+lumd add-genesis-account $(lumd keys show ibc-testbed-key -a) 1000000000000000ulum --home $IBC_TESDBED_HOME/lumd
+lumd gentx ibc-testbed-key 1000000000000ulum --home $IBC_TESDBED_HOME/lumd
+lumd collect-gentxs --home $IBC_TESDBED_HOME/lumd
