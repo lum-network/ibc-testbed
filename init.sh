5e428b9b35061ea8ed9883f2aad7c1a3f66a84ca
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-26 14:15:42 +0100

    Fix typo

--- init.sh
+++ init.sh
@@ -3,7 +3,12 @@
 ##
 
 IBC_TESDBED_HOME=$HOME/.ibc-testbed
+echo 'Testbed directory is '$IBC_TESDBED_HOME
 
+echo 'Cleaning up testbed directory...'
+rm -rf $HOME/.ibc-testbed
+
+echo 'Initializing networks keyring...'
 lumd keys add ibc-testbed-key --home $IBC_TESDBED_HOME/lumd --keyring-backend test
 osmosisd keys add ibc-testbed-key --home $IBC_TESDBED_HOME/osmosisd --keyring-backend test
 kid keys add ibc-testbed-key --home $IBC_TESDBED_HOME/kid --keyring-backend test
