f68082045d5e7ba817a593344590b83dc5e76459
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-28 12:23:52 +0100

    Make script portable

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo '[INFO] Initializing environment...'
 . ibc-testbed/.env
 
 echo '[INFO] Initializing networks...'
-if sh ibc-testbed/init.sh &>/dev/null; then
+if sh ibc-testbed/init.sh >/dev/null 2>&1; then
     echo '[INFO] Network initialized with success'
 else
     echo '[ERROR] Failed to initialize networks'
@@ -14,23 +14,23 @@ else
 fi
 
 echo '[INFO] Starting networks...'
-sh ibc-testbed/run-networks.sh &>/dev/null
+sh ibc-testbed/run-networks.sh >/dev/null 2>&1
 
 echo '[INFO] Waiting for networks to be up and running...'
 if sh ibc-testbed/wait-networks.sh &>/dev/null; then
     echo '[INFO] Networks up and running'
 else
     echo '[ERROR] Waiting for networks timed out'
-    sh ibc-testbed/stop-networks.sh &>/dev/null
+    sh ibc-testbed/stop-networks.sh >/dev/null 2>&1
     exit 1
 fi
 
 echo '[INFO] Seeding relayer wallets...'
-if sh ibc-testbed/seed-relayer.sh &>/dev/null; then
+if sh ibc-testbed/seed-relayer.sh >/dev/null 2>&1; then
     echo '[INFO] Relayer wallets seed succeeded'
 else
     echo '[ERROR] Relayer wallets seed failed'
-    sh ibc-testbed/stop-networks.sh &>/dev/null
+    sh ibc-testbed/stop-networks.sh >/dev/null 2>&1
     exit 1
 fi
 
@@ -43,6 +43,6 @@ echo '== TEST SUITE - START =='
 echo '== TEST SUITE - END =='
 
 echo 'Stopping networks...'
-sh ibc-testbed/stop-networks.sh &>/dev/null
+sh ibc-testbed/stop-networks.sh >/dev/null 2>&1
 
 exit 0
--- wait-networks.sh
+++ wait-networks.sh
@@ -7,16 +7,16 @@
 i=30
 while [ $i -ge 0 ]; do
     ok=0
-    if lumd status --node $LUM_RPC &>/dev/null; then
+    if lumd status --node $LUM_RPC >/dev/null 2>&1; then
         ok=$((ok + 1))
     fi
-    if osmosisd status --node $OSMOSIS_RPC &>/dev/null; then
+    if osmosisd status --node $OSMOSIS_RPC >/dev/null 2>&1; then
         ok=$((ok + 1))
     fi
-    if kid status --node $KI_RPC &>/dev/null; then
+    if kid status --node $KI_RPC >/dev/null 2>&1; then
         ok=$((ok + 1))
     fi
-    if gaiad status --node $COSMOS_RPC &>/dev/null; then
+    if gaiad status --node $COSMOS_RPC >/dev/null 2>&1; then
         ok=$((ok + 1))
     fi
 
