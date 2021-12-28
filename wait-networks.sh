46a1bd2e6ce5fd07e54f7842d9db5d1c2f7cec4e
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-28 12:03:04 +0100

    Fix bash incr

--- wait-networks.sh
+++ wait-networks.sh
@@ -4,27 +4,27 @@
 
 . ibc-testbed/.env
 
-i=3
+i=30
 while [ $i -ge 0 ]; do
     ok=0
     if lumd status --node $LUM_RPC &>/dev/null; then
-        let "ok++"
+        ok=$((ok + 1))
     fi
     if osmosisd status --node $OSMOSIS_RPC &>/dev/null; then
-        let "ok++"
+        ok=$((ok + 1))
     fi
     if kid status --node $KID_RPC &>/dev/null; then
-        let "ok++"
+        ok=$((ok + 1))
     fi
     if gaiad status --node $COSMOS_RPC &>/dev/null; then
-        let "ok++"
+        ok=$((ok + 1))
     fi
 
     if [[ $ok == 4 ]]; then
         exit 0
     fi
 
-    let "i--"
+    i=$((i - 1))
     sleep 1
 done
 
