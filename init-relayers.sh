9aef27e890fff6b6b183b63a66abefc305f61c3a
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-28 20:08:46 +0100

    Add delay between rly commands

--- init-relayers.sh
+++ init-relayers.sh
@@ -7,17 +7,23 @@
 echo 'Initializing Lum <> Osmosis relayer...'
 rly paths generate $LUM_CHAIN_ID $OSMOSIS_CHAIN_ID lum-osmosis --home $RELAYER_HOME
 rly tx clients lum-osmosis --home $RELAYER_HOME
+sleep 5
 rly tx connection lum-osmosis --home $RELAYER_HOME
+sleep 5
 rly tx link lum-osmosis --home $RELAYER_HOME
 
 echo 'Initializing Ki <> Osmosis relayer...'
 rly paths generate $KI_CHAIN_ID $OSMOSIS_CHAIN_ID ki-osmosis --home $RELAYER_HOME
 rly tx clients ki-osmosis --home $RELAYER_HOME
+sleep 5
 rly tx connection ki-osmosis --home $RELAYER_HOME
+sleep 5
 rly tx link ki-osmosis --home $RELAYER_HOME
 
 echo 'Initializing Cosmos <> Osmosis relayer...'
 rly paths generate $COSMOS_CHAIN_ID $OSMOSIS_CHAIN_ID cosmos-osmosis --home $RELAYER_HOME
 rly tx clients cosmos-osmosis --home $RELAYER_HOME
+sleep 5
 rly tx connection cosmos-osmosis --home $RELAYER_HOME
+sleep 5
 rly tx link cosmos-osmosis --home $RELAYER_HOME
