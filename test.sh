8fb02dfdeebd8c0e5ecff7d21c36bcead4b8b0b2
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-28 12:08:03 +0100

    Add more logging

--- test.sh
+++ test.sh
@@ -7,22 +7,28 @@ echo '[INFO] Initializing environment...'
 
 echo '[INFO] Initializing networks...'
 if sh ibc-testbed/init.sh &>/dev/null; then
+    echo '[INFO] Network initialized with success'
+else
     echo '[ERROR] Failed to initialize networks'
     exit 1
 fi
 
-echo 'Starting networks...'
+echo '[INFO] Starting networks...'
 sh ibc-testbed/run-networks.sh &>/dev/null
 
-echo 'Waiting for networks to be up and running...'
+echo '[INFO] Waiting for networks to be up and running...'
 if sh ibc-testbed/wait-networks.sh &>/dev/null; then
+    echo '[INFO] Networks up and running'
+else
     echo '[ERROR] Waiting for networks timed out'
     sh ibc-testbed/stop-networks.sh &>/dev/null
     exit 1
 fi
 
-echo 'Seeding relayer wallets...'
+echo '[INFO] Seeding relayer wallets...'
 if sh ibc-testbed/seed-relayer.sh &>/dev/null; then
+    echo '[INFO] Relayer wallets seed succeeded'
+else
     echo '[ERROR] Relayer wallets seed failed'
     sh ibc-testbed/stop-networks.sh &>/dev/null
     exit 1
