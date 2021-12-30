7fe216bd097979fa97d298e2b3d360c56c1d2cf0
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-12-30 13:10:30 +0100

    Improve tests by also sending coins from Osmosis to other networks

--- tests/test-revive-relay.sh
+++ tests/test-revive-relay.sh
@@ -6,31 +6,64 @@
 
 . ./.env
 
-echo '[INFO] Transferring coins from Lum to Osmosis (should work)...'
-if rly tx transfer $LUM_CHAIN_ID $OSMOSIS_CHAIN_ID 1ulum $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
+request_transfers() {
+    ##
+    ## Request IBC coin transfer from networks to Osmosis and from Osmosis to networks
+    ##
+    echo '[INFO] Transferring coins from Lum to Osmosis...'
+    if rly tx transfer $LUM_CHAIN_ID $OSMOSIS_CHAIN_ID 1ulum $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
+        echo "[INFO] Transaction accepted"
+    else
+        echo "[ERROR] Transaction rejected"
+        exit 1
+    fi
 
-echo '[INFO] Transferring coins from Ki to Osmosis (should work)...'
-if rly tx transfer $KI_CHAIN_ID $OSMOSIS_CHAIN_ID 1uxki $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
+    echo '[INFO] Transferring coins from Ki to Osmosis...'
+    if rly tx transfer $KI_CHAIN_ID $OSMOSIS_CHAIN_ID 1uxki $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
+        echo "[INFO] Transaction accepted"
+    else
+        echo "[ERROR] Transaction rejected"
+        exit 1
+    fi
 
-echo '[INFO] Transferring coins from Cosmos to Osmosis (should work)...'
-if rly tx transfer $COSMOS_CHAIN_ID $OSMOSIS_CHAIN_ID 1uatom $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
+    echo '[INFO] Transferring coins from Cosmos to Osmosis...'
+    if rly tx transfer $COSMOS_CHAIN_ID $OSMOSIS_CHAIN_ID 1uatom $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
+        echo "[INFO] Transaction accepted"
+    else
+        echo "[ERROR] Transaction rejected"
+        exit 1
+    fi
 
-echo '[INFO] Relay packets manually...'
+    echo '[INFO] Transferring coins from Osmosis to Lum...'
+    if rly tx transfer $OSMOSIS_CHAIN_ID $LUM_CHAIN_ID 1ulum $(lumd keys show $IBC_KEY -a --home $LUMD_HOME --keyring-backend test) --path lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
+        echo "[INFO] Transaction accepted"
+    else
+        echo "[ERROR] Transaction rejected"
+        exit 1
+    fi
+
+    echo '[INFO] Transferring coins from Osmosis to Ki...'
+    if rly tx transfer $OSMOSIS_CHAIN_ID $KI_CHAIN_ID 1ulum $(kid keys show $IBC_KEY -a --home $KID_HOME --keyring-backend test) --path ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
+        echo "[INFO] Transaction accepted"
+    else
+        echo "[ERROR] Transaction rejected"
+        exit 1
+    fi
+
+    echo '[INFO] Transferring coins from Osmosis to Cosmos...'
+    if rly tx transfer $OSMOSIS_CHAIN_ID $COSMOS_CHAIN_ID 1ulum $(gaiad keys show $IBC_KEY -a --home $GAIAD_HOME --keyring-backend test) --path cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
+        echo "[INFO] Transaction accepted"
+    else
+        echo "[ERROR] Transaction rejected"
+        exit 1
+    fi
+}
+
+# Trigger all transfers txs
+request_transfers
+
+# Relay all packets from all relays (should all work)
+echo '[INFO] Relay packets manually (all realyers should work)...'
 if rly tx relay-packets lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1 && rly tx relay-packets ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1 && rly tx relay-packets cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
     echo "[INFO] Relaying done"
 else
@@ -38,34 +71,15 @@ else
     exit 1
 fi
 
+# Relayer expiration must be high enough for the test to pass so we must wait
 echo '[INFO] Waiting 5min for the Lum <> Osmosis client to expire...'
 sleep 300
 
-echo '[INFO] Transferring coins from Lum to Osmosis (should NOT work)...'
-if rly tx transfer $LUM_CHAIN_ID $OSMOSIS_CHAIN_ID 1ulum $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
+# Trigger all transfers txs again
+request_transfers
 
-echo '[INFO] Transferring coins from Ki to Osmosis (should work)...'
-if rly tx transfer $KI_CHAIN_ID $OSMOSIS_CHAIN_ID 1uxki $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
-
-echo '[INFO] Transferring coins from Cosmos to Osmosis (should work)...'
-if rly tx transfer $COSMOS_CHAIN_ID $OSMOSIS_CHAIN_ID 1uatom $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
-
-echo '[INFO] Relay packets between Lum <> Osmosis should not work...'
+# Lum <> Osmosis relayer should not be working anymore
+echo '[INFO] Relay packets between Lum <> Osmosis (should not work)...'
 if rly tx raw update-client $OSMOSIS_CHAIN_ID $LUM_CHAIN_ID 07-tendermint-2 --home $RELAYER_HOME >/dev/null 2>&1; then
     echo "[ERROR] Relaying is supposedly working but should not be"
     exit 1
@@ -73,7 +87,8 @@ else
     echo "[INFO] Relaying not working as expected"
 fi
 
-echo '[INFO] Relay packets manually...'
+# Other relayers should work just fine and not be affected by the Lum <> Osmosis relayer issue
+echo '[INFO] Relay packets between other networks (should work)...'
 if rly tx relay-packets ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1 && rly tx relay-packets cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
     echo "[INFO] Relaying done"
 else
@@ -81,6 +96,7 @@ else
     exit 1
 fi
 
+# Launching IBC fix for Lum <> Osmosis relayer
 echo '[INFO] Creating and updating new substitute client to replace the expired one...'
 rly tx raw client $OSMOSIS_CHAIN_ID $LUM_CHAIN_ID 07-tendermint-3 --home $RELAYER_HOME >/dev/null 2>&1
 sleep 5
@@ -96,30 +112,10 @@ sleep $GOV_VOTE_DURATION
 echo '[INFO] Updating substitute client...'
 rly tx raw update-client $OSMOSIS_CHAIN_ID $LUM_CHAIN_ID 07-tendermint-3 --home $RELAYER_HOME
 
-echo '[INFO] Transferring coins from Lum to Osmosis (should work)...'
-if rly tx transfer $LUM_CHAIN_ID $OSMOSIS_CHAIN_ID 1ulum $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
+# Trigger all transfers txs again
+request_transfers
 
-echo '[INFO] Transferring coins from Ki to Osmosis (should work)...'
-if rly tx transfer $KI_CHAIN_ID $OSMOSIS_CHAIN_ID 1uxki $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
-
-echo '[INFO] Transferring coins from Cosmos to Osmosis (should work)...'
-if rly tx transfer $COSMOS_CHAIN_ID $OSMOSIS_CHAIN_ID 1uatom $(osmosisd keys show $IBC_KEY -a --home $OSMOSISD_HOME --keyring-backend test) --path cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
-    echo "[INFO] Transaction accepted"
-else
-    echo "[ERROR] Transaction rejected"
-    exit 1
-fi
-
+# Relay all packets from all relays (should all work)
 echo '[INFO] Relay packets manually...'
 if rly tx relay-packets lum-osmosis --home $RELAYER_HOME >/dev/null 2>&1 && rly tx relay-packets ki-osmosis --home $RELAYER_HOME >/dev/null 2>&1 && rly tx relay-packets cosmos-osmosis --home $RELAYER_HOME >/dev/null 2>&1; then
     echo "[INFO] Relaying done"
@@ -128,5 +124,8 @@ else
     exit 1
 fi
 
-echo '[DEBUG] Dumping test wallets (Osmosis wallet should have 3 ibc denom with 3 coins each)'
+# End test manual verification
+# Each IBC transfer should have passed
+# Even the ones done while Lum <> Osmosis relayer was out of service since we revived the relayer
+echo '[DEBUG] Dumping test wallets:\n - Osmosis wallet should have 3 ibc denom with 3 coins each\n - Each network should have an extra denom with 3 coins (uosmo IBC)'
 sh scripts/dump-wallets.sh
